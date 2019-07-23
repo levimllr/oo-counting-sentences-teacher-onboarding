d7c8011e68cad2361b8e7efd80dd30bfd684c5e9
Levi Miller <paullevimiller@gmail.com>
2019-07-22 18:09:01 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # binding.pry
+    sentences = self.split /[.?!] /
+    puts sentences
+    return sentences.length
   end
 end
